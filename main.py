4f76be8476722a3a63a6db809f48d12991cb65c1
bone0670 <56081843+bone0670@users.noreply.github.com>
2021-06-03 19:04:18 -0400

    Cleanup + remove heartbeat implementation
    Tidied up code
    Removed heartbeat thread and function since there's already a brute force reconnect in the send_json_request function

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def send_json_request(ws,request):
         ws.connect(websocket_url)
         ws.send(json.dumps(request))
 
-
 def receive_json_response(ws):
     global websocket_url
     try:
@@ -23,50 +22,35 @@ def receive_json_response(ws):
     except:
         pass
 
-
-def heartbeat(interval,ws,heartbeat_JSON):
-    while True:
-        time.sleep(interval)
-        send_json_request(ws, heartbeat_JSON)
-
-
-websocket_url = "wss://gateway.discord.gg/?v=8&encoding=json"
-channel_id = input("Enter Channel ID:")
-token = input("Enter Authorization Token:")
-os.system("cls")
-payload = {
-    "op":2,
-    "d":{
-        "token":token,
-        "properties":{
-            "$os":"windows",
-            "$browser":"chrome",
-            "$device":"pc"
+if __name__ == "__main__":
+    websocket_url = "wss://gateway.discord.gg/?v=9&encoding=json"
+    channel_id = input("Enter Channel ID:")
+    token = input("Enter Authorization Token:")
+    os.system("cls")
+    payload = {
+        "op":2,
+        "d":{
+            "token":token,
+            "properties":{
+                "$os":"windows",
+                "$browser":"chrome",
+                "$device":"pc"
+            }
         }
     }
-}
-heartbeat_JSON = {
-    "op": 1,
-    "d": "null"
-}
 
-ws = websocket.WebSocket()
-ws.connect(websocket_url)
+    ws = websocket.WebSocket()# Create new websocket
+    ws.connect(websocket_url)# Connect to the websocket
 
-event = receive_json_response(ws)
-heartbeat_interval = event["d"]["heartbeat_interval"] / 1000 * random.random()
-threading.Thread(target=heartbeat,args=(heartbeat_interval,ws,heartbeat_JSON)).start()
+    send_json_request(ws,payload)# Send initial identify payload
 
-send_json_request(ws,payload)
-
-while True:
-    event = receive_json_response(ws)
-    if event["op"]==1:
-        send_json_request(ws,heartbeat_JSON)
-    try:
-        if event["d"]["channel_id"]==channel_id:
-            content = event["d"]["content"]
-            author = event["d"]["author"]["username"]
-            print(f"{author}: {content}\n")
-    except:
-        pass
+    while True:
+        event = receive_json_response(ws)
+        try:
+            if event["d"]["channel_id"] == channel_id:
+                # print(json.dumps(event,indent = 5))# Test print message to see the structure of the payload better
+                content = event["d"]["content"]
+                author = event["d"]["author"]["username"]
+                print(f"{author}: {content}\n")
+        except:
+            pass
